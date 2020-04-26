18540e2d48c4423b4cb473664a8d17f8acfc87a5
Marysia Pabich <marysia.pabich@gmail.com>
2020-04-26 16:31:38 +0200

    Add before/after photo effect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import BeforeAfterSlider from 'react-before-after-slider';
+import before from './images/before.jpeg';
+import after from './images/after.jpeg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <BeforeAfterSlider
+        before={before}
+        after={after}
+        width={640}
+        height={480}
+      />
     </div>
   );
 }
