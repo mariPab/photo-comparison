f4c366806c776b3af94e8fec37bdafd747103d11
Marysia Pabich <marysia.pabich@gmail.com>
2020-04-26 18:04:14 +0200

    Updates

---
+++ precache-manifest.fc8a18fdc872bf994ebc6051071bf3e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f60cd1718addba89ba8ad24be8842b6",
+    "url": "/photo-comparison/index.html"
+  },
+  {
+    "revision": "6497a2cac8109cf154a0",
+    "url": "/photo-comparison/static/css/main.55566754.chunk.css"
+  },
+  {
+    "revision": "14d4157c76e850e2c464",
+    "url": "/photo-comparison/static/js/2.6535107d.chunk.js"
+  },
+  {
+    "revision": "d4b34d94339d595a5fcaefa694b42d2f",
+    "url": "/photo-comparison/static/js/2.6535107d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6497a2cac8109cf154a0",
+    "url": "/photo-comparison/static/js/main.46cd6d97.chunk.js"
+  },
+  {
+    "revision": "11ea60c3887a1304f9e3",
+    "url": "/photo-comparison/static/js/runtime-main.43f8aa99.js"
+  },
+  {
+    "revision": "d6516d13e539b3adaf21dc51df848c1a",
+    "url": "/photo-comparison/static/media/after.d6516d13.jpeg"
+  },
+  {
+    "revision": "149c7e83752907d927b8aa72593017a2",
+    "url": "/photo-comparison/static/media/before.149c7e83.jpeg"
+  }
+]);
