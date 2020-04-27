8a8dff646c2c386d1e3910e7a62b5eff892f96ec
Marysia Pabich <marysia.pabich@gmail.com>
2020-04-27 22:41:08 +0200

    Add React Router, reorganize app architecture

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import BeforeAfterSlider from 'react-before-after-slider';
-import before from './images/before.jpeg';
-import after from './images/after.jpeg';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import { MainLayout } from './components/layout/MainLayout/MainLayout';
+import { Photo } from './components/views/Photo/Photo';
+// import { Submit } from './components/views/Submit/Submit';
+import { MAIN_PAGE_URL } from './config';
 
 function App() {
   return (
-    <div className="wrapper">
-      <BeforeAfterSlider
-        before={before}
-        after={after}
-        width={640}
-        height={480}
-      />
-    </div>
+    <MainLayout>
+      <Switch>
+        <Route path={`${MAIN_PAGE_URL}/`} exact to component={Photo} />
+        {/* <Route path="/submit" exact component={Submit} /> */}
+      </Switch>
+    </MainLayout>
+
   );
 }
 
