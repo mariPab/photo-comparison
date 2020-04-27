13f90b5c76557b03d8e5b769f39ac5cf2ba4e017
Marysia Pabich <marysia.pabich@gmail.com>
2020-04-27 21:00:03 +0200

    Add redux store, basic saga config, wrap app into router

---
+++ src/redux/photoRedux.js
@@ -0,0 +1,10 @@
+const initialState = {
+  data: [],
+};
+
+export default function reducer(statePart = initialState, action = {}) {
+  switch (action.type) {
+    default:
+      return statePart;
+  }
+}
---
+++ src/redux/store.js
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import photoReducer from './photoRedux.js';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './sagas';
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  photoReducer,
+  compose(
+    applyMiddleware(sagaMiddleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+  )
+);
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
