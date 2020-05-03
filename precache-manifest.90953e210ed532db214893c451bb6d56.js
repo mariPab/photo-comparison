a220669c0dbccf9926808ee01027fedafdd89219
Marysia Pabich <marysia.pabich@gmail.com>
2020-05-03 11:40:48 +0200

    Updates

---
+++ precache-manifest.90953e210ed532db214893c451bb6d56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2180ca7fa474a42b8d1756a363ac075d",
+    "url": "/photo-comparison/admin/index.html"
+  },
+  {
+    "revision": "f1dad75880e731b866cb",
+    "url": "/photo-comparison/admin/static/css/main.b038c233.chunk.css"
+  },
+  {
+    "revision": "879405465d96cc2642bc",
+    "url": "/photo-comparison/admin/static/js/2.58d5ec6a.chunk.js"
+  },
+  {
+    "revision": "8fca96f93226e21975465a36b3f6d5e9",
+    "url": "/photo-comparison/admin/static/js/2.58d5ec6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1dad75880e731b866cb",
+    "url": "/photo-comparison/admin/static/js/main.e558cb3f.chunk.js"
+  },
+  {
+    "revision": "deeab154b9e1fe0b8c53",
+    "url": "/photo-comparison/admin/static/js/runtime-main.ffa515d6.js"
+  }
+]);
