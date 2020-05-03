cdcea2e1e4e1b708210eea44e450ee8fc7ae9af7
Marysia Pabich <marysia.pabich@gmail.com>
2020-05-03 16:59:48 +0200

    Updates

---
+++ precache-manifest.675aa3de8fba34c2fc8975b86dedee66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23e9c8e1b597d93e202f5b72e3fe00f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "585c4e1df39eec663aec",
+    "url": "/static/css/main.436d9cd5.chunk.css"
+  },
+  {
+    "revision": "4433f5a7e63853542bd0",
+    "url": "/static/js/2.5618c657.chunk.js"
+  },
+  {
+    "revision": "8fca96f93226e21975465a36b3f6d5e9",
+    "url": "/static/js/2.5618c657.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "585c4e1df39eec663aec",
+    "url": "/static/js/main.130c59e0.chunk.js"
+  },
+  {
+    "revision": "af99dfa0dc897c2d75a7",
+    "url": "/static/js/runtime-main.0d240175.js"
+  }
+]);
