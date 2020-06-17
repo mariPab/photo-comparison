6c7b249a2649f2290fb0c948c144acafc60e4657
Marysia Pabich <marysia.pabich@gmail.com>
2020-06-17 17:55:57 +0200

    Add mutler middleware to endpoints that contains form data

--- routes/photos.routes.ts
+++ routes/photos.routes.ts
@@ -1,13 +1,13 @@
 import * as express from 'express';
-import { upload, photosFields } from '../settings/mutlerConfig';
-import { photos } from '../controllers/photos.controller';
+import multerConfig from '../settings/multerConfig';
+import photos from '../controllers/photos.controller';
 
 const router = express.Router();
 
 router.get('/all', photos.loadAll);
 router.get('/photos/:id', photos.loadById);
-router.post('/submit', /* upload.fields(photosFields), */ photos.submit);
+router.post('/submit', multerConfig.upload.fields(multerConfig.photosFields), photos.submit);
 router.delete('/photos/:id', photos.deleteById);
-router.put('/photos/:id', photos.editPhotoComparison);
+router.put('/photos/:id', multerConfig.upload.fields(multerConfig.photosFields), photos.editPhotoComparison);
 
 module.exports = router;
