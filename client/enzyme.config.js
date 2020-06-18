1c057366c613f8fbfc17faa597c5555d4ef877ab
Marysia Pabich <marysia.pabich@gmail.com>
2020-06-18 23:19:33 +0200

    Configure Jest env for client

---
+++ client/enzyme.config.js
@@ -0,0 +1,8 @@
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+console.error = err => {
+  throw new Error(err);
+};
+
+configure({ adapter: new Adapter() });
---
+++ client/jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  testPathIgnorePatterns: ["/lib/", "/node_modules/"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  collectCoverage: true,
+  moduleFileExtensions: [
+    'web.js',
+    'js',
+    'web.ts',
+    'ts',
+    'web.tsx',
+    'tsx',
+    'json',
+    'web.jsx',
+    'jsx',
+    'node',
+    'svg',
+  ],
+
+};
