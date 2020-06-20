e263e6dae87fe5807b7237a2ad4e7a99ebd23cdf
Marysia Pabich <marysia.pabich@gmail.com>
2020-06-20 20:22:13 +0200

    Code refactor for MainLayout component, add basic tests

--- client/src/components/layout/MainLayout/MainLayout.test.tsx
+++ client/src/components/layout/MainLayout/MainLayout.test.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { MainLayoutComponent } from '.';
 
-describe('Component MainLayout', () => {
+describe('MainLayout pure component', () => {
   it('should render without crashing', () => {
-    const component = shallow(<MainLayoutComponent />);
+    const component = shallow(
+      <MainLayoutComponent>
+        <div></div>
+      </MainLayoutComponent>);
     expect(component).toBeTruthy();
   });
 });
--- client/src/components/layout/MainLayout/index.tsx
+++ client/src/components/layout/MainLayout/index.tsx
@@ -1,16 +1,13 @@
-import React, { FunctionComponent } from 'react';
-import styles from './MainLayout.module.scss';
+import React from 'react';
 
 interface Props {
-  children: React.ReactElement
+  children: React.ReactElement;
 }
-const Component: FunctionComponent<Props> = ({ children }: Props) => {
-  return (
-    <div className={styles.root}>
-      {children}
-    </div>
-  );
-};
+const Component: React.FunctionComponent<Props> = ({ children }: Props) => (
+  <div>
+    {children}
+  </div>
+);
 export {
   Component as MainLayout,
   Component as MainLayoutComponent,
