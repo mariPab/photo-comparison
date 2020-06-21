486fe36fa10dc8b3b2aa4ba351d7f5b8c50a8a5e
Marysia Pabich <marysia.pabich@gmail.com>
2020-06-21 13:50:45 +0200

    Test editComparison & submitPhotos sagas

--- client/src/redux/photos/__tests__/saga.test.ts
+++ client/src/redux/photos/__tests__/saga.test.ts
@@ -3,6 +3,7 @@ import {
   getAll,
   getPhotoData,
   submitPhotos,
+  editComparison,
   deleteComparison
 } from '../saga';
 import {
@@ -10,13 +11,14 @@ import {
   GET_PHOTO,
   SET_PHOTO,
   SUBMIT_PHOTOS,
+  EDIT_COMPARISON,
   DELETE_COMPARISON,
   GET_ALL,
 } from '../types';
 import axios from 'axios';
 import { API_URL } from '../../../config';
-import { all, fork, takeEvery, put } from 'redux-saga/effects';
-import { FormDataPayload } from '../types';
+import { put } from 'redux-saga/effects';
+
 const mockPhotoData = {
   _id: '635736yb4574',
   title: 'title',
@@ -65,6 +67,59 @@ describe('Photos Saga - ', () => {
       expect(result).toEqual(put({
         type: SET_PHOTO,
         payload: { ...mockRes.data },
+      }));
+    });
+    it('and then nothing', result => {
+      expect(result).toBeUndefined();
+    });
+  });
+  describe('submitPhotos', () => {
+    const formData = new FormData();
+    const payload = { data: formData };
+    const mockArgs = {
+      type: SUBMIT_PHOTOS,
+      payload: payload
+    };
+    const it = sagaHelper(submitPhotos(mockArgs));
+    it('Should request for desired API call', apiReq => {
+      expect(apiReq).toEqual(
+        axios.post(`${API_URL}/submit`,
+          payload.data, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }))
+    });
+    it('then trigger GET_ALL action', result => {
+      expect(result).toEqual(put({
+        type: GET_ALL,
+      }));
+    });
+    it('and then nothing', result => {
+      expect(result).toBeUndefined();
+    });
+  });
+  describe('editComparison', () => {
+    const formData = new FormData();
+    const payload = { data: formData, id: mockPhotoData._id };
+    const mockArgs = {
+      type: EDIT_COMPARISON,
+      payload: payload
+    };
+    const it = sagaHelper(editComparison(mockArgs));
+    it('Should request for desired API call', apiReq => {
+      expect(apiReq).toEqual(
+        axios.put(`${API_URL}/photos/${payload.id}`,
+          payload.data, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }))
+    });
+    it('then trigger SET_PHOTO action', result => {
+      expect(result).toEqual(put({
+        type: SET_PHOTO,
+        payload: { ...mockPhotoData },
       }));
     });
     it('and then nothing', result => {
