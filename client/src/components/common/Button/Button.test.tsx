a1d4b60d33c261413650d44744ecb77b57438b9e
Marysia Pabich <marysia.pabich@gmail.com>
2020-06-30 21:50:03 +0200

    Add test cases for Button component

--- client/src/components/common/Button/Button.test.tsx
+++ client/src/components/common/Button/Button.test.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, ShallowWrapper } from 'enzyme';
 import { Button } from '.';
 
+const mockClbck = jest.fn();
+
 describe('Component Button', () => {
-  describe('without variant', () => {
+  it('without any prop should render without crashing', () => {
+    const component = shallow(<Button />);
+    expect(component).toBeTruthy();
+  });
+  it('with "fab" variant should render without crashing', () => {
+    const component = shallow(<Button variant="fab" />);
+    expect(component).toBeTruthy();
+  });
+  describe('with onClick prop', () => {
+    let component: ShallowWrapper<{}>
+    beforeEach(() => {
+      component = shallow(<Button onClick={mockClbck} />);
+    });
     it('should render without crashing', () => {
-      const component = shallow(<Button />);
       expect(component).toBeTruthy();
     });
-  });
-  describe('with "fab" variant', () => {
-    it('should render without crashing', () => {
-      const component = shallow(<Button variant="fab" />);
-      expect(component).toBeTruthy();
+    it('should call callback funtion on click', () => {
+      component.simulate('click');
+      expect(mockClbck).toHaveBeenCalledTimes(1);
     });
   });
+
 });
