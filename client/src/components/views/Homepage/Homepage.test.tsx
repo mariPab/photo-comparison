312dc4737d8d47f234d4174c7afbaf9785b57db5
Marysia Pabich <marysia.pabich@gmail.com>
2020-07-15 20:07:21 +0200

    Add required props for proper rendering shallow component in test

--- client/src/components/views/Homepage/Homepage.test.tsx
+++ client/src/components/views/Homepage/Homepage.test.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { HomepageComponent } from '.';
+import { createMemoryHistory, createLocation } from 'history';
+import { match } from 'react-router';
 
+const history = createMemoryHistory();
+const path = `/random`;
+const customMatch: match<{}> = {
+  isExact: false,
+  path,
+  url: path,
+  params: {},
+};
+const location = createLocation(customMatch.url);
+
+const mockProps = {
+  redirectToRandomPhoto: jest.fn(),
+  history: history,
+  location: location,
+  match: customMatch,
+};
 describe('Component Homepage', () => {
   it('should render without crashing', () => {
-    const component = shallow(<HomepageComponent />);
+    const component = shallow(<HomepageComponent {...mockProps} />);
     expect(component).toBeTruthy();
   });
 });
