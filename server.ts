22e58db01d8a9bd6e7e758928d1f56965bb61cc7
Marysia Pabich <marysia.pabich@gmail.com>
2020-10-24 09:39:29 +0200

    Restore to previous config

---
+++ server.ts
@@ -0,0 +1,57 @@
+import { Response, Request, Application } from 'express/index';
+import express = require('express');
+import cors = require('cors');
+import path = require('path');
+import mongoose = require('mongoose');
+import router from './routes/photos.routes';
+
+const app: Application = express();
+
+/* MIDDLEWARE */
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+/* API ENDPOINTS */
+app.use('/api', router);
+app.use(express.static(path.join(__dirname, '/public')));
+
+
+/* REACT WEBSITE */
+if (process.env.NODE_ENV === 'production') {
+  app.get('*', (_req: Request, res: Response): void => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'), (err: Error) => {
+      if (err) res.status(500).send(err);
+    });
+  });
+}
+
+app.use((_req: Request, res: Response): void => {
+  res.status(404).send({ message: 'not found...' });
+});
+process.env.NODE_ENV === 'production' ?
+  mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PW}@cluster0-314sb.mongodb.net/PhotoComparison?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }) :
+  mongoose.connect(
+    'mongodb://localhost:27017/photo-comparison',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+const db = mongoose.connection;
+
+db.once('open', (): void => {
+  console.log('Successfully connected to the database');
+});
+db.on('error', (err: Error): void => console.log('Error: ' + err));
+
+/* START SERVER */
+const port = process.env.PORT || 8000;
+export const server = app.listen(port, (): void => {
+  console.log('Server is running on port: ' + port);
+});
